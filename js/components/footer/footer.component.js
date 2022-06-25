49867b293e172dea192c175a28ff39a1e83eb29a
LAZ-R <crazydrawer62@gmail.com>
2022-06-25 20:34:51 +0200

    v 1.2

--- js/components/footer/footer.component.js
+++ js/components/footer/footer.component.js
@@ -2,7 +2,7 @@ export const render = () => {
     const footer = document.createElement('footer');
     footer.innerHTML =
         '<div>' +
-            '&copy; 2022 - v 1.1 - laz_R - <a href="./about.html">About</a>' +
+            '&copy; 2022 - v 1.2 - laz_R - <a href="./about.html">About</a>' +
         '</div>';
 
     document.getElementById('body').appendChild(footer);
