1d901ab747d5038048096fda4a272c7928172d17
LAZ-R <crazydrawer62@gmail.com>
2022-06-26 17:38:39 +0200

    v 1.14 : maskable on everything

--- js/components/footer/footer.component.js
+++ js/components/footer/footer.component.js
@@ -2,7 +2,7 @@ export const render = () => {
     const footer = document.createElement('footer');
     footer.innerHTML =
         '<div>' +
-            '&copy; 2022 - v 1.13 - <a href="https://laz-r.github.io/">laz_R</a> - <a href="./about.html">About</a>' +
+            '&copy; 2022 - v 1.14 - <a href="https://laz-r.github.io/">laz_R</a> - <a href="./about.html">About</a>' +
         '</div>';
 
     document.getElementById('body').appendChild(footer);
